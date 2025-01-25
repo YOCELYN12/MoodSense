bb6bb2a0b83d7e6e28994b46ac68370f79ed4ff3
Javier <jjaviervgg@gmail.com>
2025-01-26 00:39:49 +0100

    Añadidura

--- Proyecto_Mood_Sense/src/context/Context.jsx
+++ Proyecto_Mood_Sense/src/context/Context.jsx
@@ -144,6 +144,22 @@ export const AuthContextProvider = ({ children }) => {
     }
   };
 
+
+  const GetUserTable = async (userId) => {
+      const { data, error } = await supabase
+        .from('user') // nombre de tu tabla
+        .select('*') // selecciona todas las columnas
+        .eq('id', userId) // condicionante, en este caso 'id'
+        .single(); // Devuelve un único registro
+    
+      if (error) {
+        console.error("Error:", error);
+      } else {
+        console.log("User data:", data);
+      }
+    
+  }
+
   const logOut = async () => {
     //Sale del perfil
     try {
@@ -204,7 +220,7 @@ export const AuthContextProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider
-      value={{ user, objPerfil, Loading, getUserInfo, signUp, asignIn, logOut }}
+      value={{ user, objPerfil, Loading, getUserInfo, signUp, asignIn, logOut, GetUserTable }}
     >
       {children}
     </AuthContext.Provider>
