f3f6d07e667607fe7f8a41c74843a5fce88e3faf
Alexander <alexanderaguilar726@gmail.com>
2025-01-25 23:56:02 -0600

    Estructura admin

--- Proyecto_Mood_Sense/src/Pages/Admin/Admin.jsx
+++ Proyecto_Mood_Sense/src/Pages/Admin/Admin.jsx
@@ -18,43 +18,23 @@ const Admin = () => {
       <div>
         <input type="date" name="" id="" placeholder="Filtrar por fecha" />
       </div>
-      <div className="grid grid-cols-2 gap-4 p-4 bg-gray-50 min-h-screen">
-      {/* Card: Enojo */}
-      <div className="relative bg-purple-50 border-2 border-purple-400 rounded-2xl p-4 flex flex-col items-center">
-        <div className="absolute top-2 right-2 text-xs text-purple-600 font-medium bg-purple-100 px-2 py-1 rounded-md">
-          16% <span className="text-orange-500">↗</span>
-        </div>
-        <div className="text-purple-600 text-lg font-medium mb-2">Enojo</div>
-        <div className="text-4xl font-bold text-gray-800">12</div>
+      <div>
+        <p>Enojo</p>
+        <p>12</p>
       </div>
-
-      {/* Card: Alegre */}
-      <div className="relative bg-purple-50 rounded-2xl p-4 flex flex-col items-center">
-        <div className="absolute top-2 right-2 text-xs text-purple-600 font-medium bg-purple-100 px-2 py-1 rounded-md">
-          16% <span className="text-orange-500">↘</span>
-        </div>
-        <div className="text-purple-600 text-lg font-medium mb-2">Alegre</div>
-        <div className="text-4xl font-bold text-gray-800">20</div>
+      <div>
+        <p>Alegre</p>
+        <p>20</p>
       </div>
-
-      {/* Card: Miedo */}
-      <div className="relative bg-purple-50 rounded-2xl p-4 flex flex-col items-center">
-        <div className="absolute top-2 right-2 text-xs text-purple-600 font-medium bg-purple-100 px-2 py-1 rounded-md">
-          16% <span className="text-orange-500">↘</span>
-        </div>
-        <div className="text-purple-600 text-lg font-medium mb-2">Miedo</div>
-        <div className="text-4xl font-bold text-gray-800">2</div>
+      <div>
+        <p>Miedo</p>
+        <p>2</p>
       </div>
-
-      {/* Card: Triste */}
-      <div className="relative bg-purple-50 rounded-2xl p-4 flex flex-col items-center">
-        <div className="absolute top-2 right-2 text-xs text-purple-600 font-medium bg-purple-100 px-2 py-1 rounded-md">
-          16% <span className="text-orange-500">↗</span>
-        </div>
-        <div className="text-purple-600 text-lg font-medium mb-2">Triste</div>
-        <div className="text-4xl font-bold text-gray-800">4</div>
+      <div>
+        <p>Triste</p>
+        <p>4</p>
       </div>
-    </div>
+      <div></div>
     </div>
   );
 };
