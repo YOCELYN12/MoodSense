d3e710e74e3fba9c685e7bc71a6cd04370405b48
Yos12 <yocelynrivera9@gmail.com>
2025-01-25 23:50:07 -0600

    Cambios en el archivo

--- Proyecto_Mood_Sense/src/Components/service/service.js
+++ Proyecto_Mood_Sense/src/Components/service/service.js
@@ -1,7 +1,7 @@
 
 
 async function getAudio() {
-    try {
+   try {
       const response = await fetch("http://localhost:3000/datosmeta");
       const data = await response.json();
       return data;
