9ba2e98b5bfdb4c31222d0aa20d0c0f41c0f41ef
Javier <jjaviervgg@gmail.com>
2025-01-26 06:27:19 +0100

    DB_JSON actualizado

---
+++ Proyecto_Mood_Sense/src/Components/service/service.js
@@ -0,0 +1,110 @@
+
+
+async function getDatosMeta() {
+    try {
+      const response = await fetch("http://localhost:3000/datosmeta");
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error al obtener el usuario:", error);
+      return [];
+    }
+  }
+
+  async function postDatosMetadata(data) {
+    try {
+  
+      const response = await fetch("http://localhost:3000/datosmeta", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error al registrar los datos meta: ${response.statusText}`);
+      }
+  
+    } catch (error) {
+      console.error("Error al enviar el usuario al backend:", error);
+      return null;
+    }
+  }
+
+  async function getInstitution() {
+    try {
+      const response = await fetch("http://localhost:3000/institution");
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error al obtener la institution:", error);
+      return [];
+    }
+  }
+
+
+  async function postInstitution(institution) {
+    try {
+  
+      const response = await fetch("http://localhost:3000/institution", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(institution),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error al registrar el usuario: ${response.statusText}`);
+      }
+  
+    } catch (error) {
+      console.error("Error al enviar el usuario al backend:", error);
+      return null;
+    }
+  }
+
+
+  async function postUser(user) {
+    try {
+  
+      const response = await fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error al registrar el usuario: ${response.statusText}`);
+      }
+  
+    } catch (error) {
+      console.error("Error al enviar el usuario al backend:", error);
+      return null;
+    }
+  }
+
+  async function postUser(user) {
+    try {
+  
+      const response = await fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error al registrar el usuario: ${response.statusText}`);
+      }
+  
+    } catch (error) {
+      console.error("Error al enviar el usuario al backend:", error);
+      return null;
+    }
+  }
+
